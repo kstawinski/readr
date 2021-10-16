d5bc7e68d30069c73e53aad47a1b3545d185480b
Kacper Stawiński <stawinskidev@gmail.com>
2021-10-16 23:31:18 +0200

    feat(books): use Book component

--- src/views/Books.svelte
+++ src/views/Books.svelte
@@ -4,13 +4,23 @@
   import { Books } from '../hooks/Books'
   import { Store } from '../hooks/Store'
 
+  import Book from '../lib/Book.svelte'
+
+  import { Grid, Row, Column } from 'carbon-components-svelte'
+
   let books: BooksArray = []
   Books.getAllBooks(Store.getItem('uid')).then(res => books = res)
 </script>
 
 <main>
   <h1>{PAGE_TITLE}</h1>
-  {#each books as book}
-    {book.title}
-  {/each}
+  <Grid>
+    <Row>
+      {#each books as book}
+        <Column sm={1}>
+          <Book {book} />
+        </Column>
+      {/each}
+    </Row>
+  </Grid>
 </main>
