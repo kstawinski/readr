67b12c58cf9fdf86f58982f29220c3314df0e0de
Kacper Stawiński <stawinskidev@gmail.com>
2021-10-16 17:53:03 +0200

    feat(routing): create views

---
+++ src/views/Home.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const PAGE_TITLE: string = 'Home'
+</script>
+
+<main>
+  <h1>{PAGE_TITLE}</h1>
+</main>
---
+++ src/views/Login.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const PAGE_TITLE: string = 'Login'
+</script>
+
+<main>
+  <h1>{PAGE_TITLE}</h1>
+</main>
