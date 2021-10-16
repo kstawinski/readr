56f4cf4acda695602b0199f2ade596c0dc00cb2f
Kacper Stawiński <stawinskidev@gmail.com>
2021-10-16 20:48:17 +0200

    feat(login): rewrite login system using `Auth` and `Store` hooks

--- src/views/Login.svelte
+++ src/views/Login.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
   const PAGE_TITLE: string = 'Login'
 
-  import { auth, provider } from '../firebase'
-  import { signInWithPopup } from 'firebase/auth'
-  import { authState } from 'rxfire/auth'
+  import { Auth } from '../hooks/Auth'
+  import { Store } from '../hooks/Store'
+  import { navigate } from 'svelte-routing'
   import { ButtonSet, Button } from 'carbon-components-svelte'
   import LogoGoogle16 from 'carbon-icons-svelte/lib/LogoGoogle16'
 
-  let user;
-
-  const unsubscribe = authState(auth).subscribe(u => user = u);
-
-  function login () {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        console.log(result)
-      })
+  const login = () => {
+    Auth.signInUsingGoogle()
+      .then(({ uid }) => Store.setItem('uid', uid)
+        .then(() => navigate('/books', { replace: true }))
+      )
   }
 </script>
 
