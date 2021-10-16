7a066100bcc2d9c072f300a7e72766cba7aa5463
Kacper Stawiński <stawinskidev@gmail.com>
2021-10-16 21:18:47 +0200

    feat(authproxy): add "logged in - not logged in" proxy

---
+++ src/views/AuthProxy.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Route } from 'svelte-routing'
+  import { Auth } from '../hooks/Auth'
+  import AuthError from './AuthError.svelte'
+
+  export let path
+  export let component
+
+  const isAuthenticated = Auth.isLoggedIn()
+</script>
+
+{#if isAuthenticated}
+  <Route path={path} component={component} />
+{:else}
+  <Route path={path} component={AuthError} />
+{/if}
