1a4446ea23e27edd1cd465fb81ddf75d7e634b8b
Kacper Stawiński <stawinskidev@gmail.com>
2021-10-16 23:07:46 +0200

    feat(Books): set types

--- src/hooks/Books.ts
+++ src/hooks/Books.ts
@@ -3,13 +3,14 @@ import { collection, getDocs, where, query } from 'firebase/firestore'
 
 export const Books = {
   getAllBooks: (uid: string) => new Promise((resolve, reject) => {
-    console.log(uid)
-    const booksRef = query(collection(db, 'books'), where('uid', '==', uid))
+    const booksRef = query(
+      collection(db, 'books'),
+      where('uid', '==', uid)
+    )
 
-    return getDocs(booksRef).then(qSnap => {
-      return resolve(
-        qSnap.docs.map(d => ({ id: d.id, ...d.data() }))
-      )
+    getDocs(booksRef).then(qSnap => {
+      const booksArray: BooksArray = qSnap.docs.map(d => (<Book>{ id: d.id, ...d.data() }))
+      resolve(booksArray)
     })
   })
 }
