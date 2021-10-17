deeb5cd1e20a8356fb5a368caa55788e0ba81f55
Kacper Stawiński <stawinskidev@gmail.com>
2021-10-17 13:04:37 +0200

    feat(add): create form

--- src/views/Add.svelte
+++ src/views/Add.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
   const PAGE_TITLE: string = 'Add book'
+
+  import { Button, Tile, TextInput } from 'carbon-components-svelte'
+  import Search16 from 'carbon-icons-svelte/lib/Search16'
 </script>
 
 <main>
   <h1>{PAGE_TITLE}</h1>
+
+  <Tile>
+    <p>Dodaj ksiażkę, wprowadzając numer ISBN znajdujący się z tyłu okładki.</p>
+    
+    <div class="add__field">
+      <TextInput labelText="Numer ISBN" />
+    </div>
+
+    <Button kind="secondary" icon={Search16}>Szukaj książki</Button>
+  </Tile>
 </main>
+
+<style>
+  .add__field {
+    margin: 15px 0 25px 0;
+  }
+</style>
