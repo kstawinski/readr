fee26457db02589cdd637e80e98bf290a7746252
Kacper Stawiński <stawinskidev@gmail.com>
2021-10-17 13:30:07 +0200

    feat(add): fetch book data

--- src/views/Add.svelte
+++ src/views/Add.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
   const PAGE_TITLE: string = 'Dodaj książkę'
 
-  import { Button, Tile, TextInput } from 'carbon-components-svelte'
+  import { Button, Tile, TextInput, InlineLoading, Row } from 'carbon-components-svelte'
   import Search16 from 'carbon-icons-svelte/lib/Search16'
+  import axios from 'axios'
+
+  let ISBN: number
+  let isFetching = false
+
+  const searchUsingISBN = () => {
+    if (ISBN && ISBN.toString().length === 13) {
+      isFetching = true
+      axios.get(
+        `https://cors-anywhere.herokuapp.com/https://data.bn.org.pl/api/bibs.json?isbnIssn=${ISBN}`,
+        { timeout: 10000 })
+        .then(data => data.data.bibs[0])
+        .then(book => {
+          console.log(book)
+          isFetching = false
+        })
+    }
+    else {
+      alert('Niepoprawna dlugosc numer isbn')
+      return
+    }
+  }
 </script>
 
 <main>
@@ -12,15 +34,37 @@
     <p>Dodaj ksiażkę, wprowadzając numer ISBN znajdujący się z tyłu okładki.</p>
     
     <div class="add__field">
-      <TextInput labelText="Numer ISBN" />
+      <TextInput labelText="Numer ISBN" bind:value={ISBN} />
     </div>
 
-    <Button kind="secondary" icon={Search16}>Szukaj książki</Button>
+    <div class="add__footer">
+      <Button
+        kind="secondary"
+        icon={Search16}
+        on:click={searchUsingISBN}
+        disabled={isFetching}
+      >Szukaj książki</Button>
+
+      {#if isFetching}
+        <div class="add__loader">
+          <InlineLoading />
+        </div>
+      {/if}
+    </div>
   </Tile>
 </main>
 
 <style>
   .add__field {
     margin: 15px 0 25px 0;
+  }
+
+  .add__footer {
+    display: flex;
+    align-items: center;
+  }
+
+  .add__loader {
+    margin-left: 20px;
   }
 </style>
