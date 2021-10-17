337cf567f3b111036bb7311c0efd48775e88d562
Kacper Stawiński <stawinskidev@gmail.com>
2021-10-17 12:56:17 +0200

    feat(books): add new book button

--- src/views/Books.svelte
+++ src/views/Books.svelte
@@ -2,13 +2,16 @@
   const PAGE_TITLE: string = 'Books'
 
   import { Books } from '../hooks/Books'
-
   import Book from '../lib/Book.svelte'
 
-  import { Grid, Row, Column } from 'carbon-components-svelte'
+  import BookmarkAdd16 from 'carbon-icons-svelte/lib/BookmarkAdd16'
+  import { Grid, Row, Column, Button } from 'carbon-components-svelte'
+  import { navigate } from 'svelte-routing'
 
   let books: BooksArray = []
   Books.getAllBooks().then(res => books = res)
+
+  const redirectToAddView = () => navigate('/add', { replace: true })
 </script>
 
 <main>
@@ -22,4 +25,21 @@
       {/each}
     </Row>
   </Grid>
+
+  <div class="books__footer">
+    <Button
+      icon={BookmarkAdd16}
+      kind="secondary"
+      on:click={redirectToAddView}
+    >Dodaj kolejną książkę</Button>
+  </div>
 </main>
+
+<style>
+  .books__footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    margin: 20px;
+  }
+</style>
