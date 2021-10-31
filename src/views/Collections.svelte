63b372f26f8ea1be25c36a4b2e4b5be11c501f70
Kacper Stawiński <stawinskidev@gmail.com>
2021-10-31 13:22:56 +0100

    feat(collections): add modal and functionality

--- src/views/Collections.svelte
+++ src/views/Collections.svelte
@@ -6,12 +6,21 @@
   import {
     InlineLoading,
     Content,
-    Tile
+    Tile,
+    Button,
+    ComposedModal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    TextInput
   } from 'carbon-components-svelte'
+  import FolderAdd16 from 'carbon-icons-svelte/lib/FolderAdd16'
   import { Collections } from '../hooks/Collections'
   import Header from '../lib/Header.svelte'
 
   let collections: CollectionsArray = []
+  let isAddModalVisible = false
+  let collectionName = ''
 
   const fetchCollections = () => {
     Collections.getAll()
@@ -21,10 +30,41 @@
   }
 
   onMount(() => fetchCollections())
+
+  const addCollection = (name: string) => {
+    Collections.create(name)
+      .then(() => {
+        fetchCollections()
+        isAddModalVisible = false
+      })
+      .catch(error => console.error(error))
+  }
 </script>
 
 <main>
   <Header title={PAGE_TITLE} />
+
+  {#if isAddModalVisible}
+  <ComposedModal bind:open={isAddModalVisible}>
+    <ModalHeader title="Nowa kolekcja" />
+    <ModalBody hasForm>
+      <TextInput
+        size="xl"
+        hideLabel
+        placeholder="Wprowadź nazwę kolekcji..."
+        bind:value={collectionName}
+      />
+    </ModalBody>
+    <ModalFooter>
+      <Button kind="secondary">Anuluj</Button>
+      <Button
+        kind="primary"
+        disabled={!(collectionName.length > 3)}
+        on:click={() => addCollection(collectionName)}
+      >Dodaj kolekcję</Button>
+    </ModalFooter>
+  </ComposedModal>
+  {/if}
 
   <Content>
     {#each collections as collection}
@@ -37,6 +77,14 @@
       </div>
     {/each}
   </Content>
+
+  <div class="collections__footer">
+    <Button
+      icon={FolderAdd16}
+      kind="secondary"
+      on:click={ () => isAddModalVisible = true }
+    >Utwórz nową kolekcję</Button>
+  </div>
 </main>
 
 <style lang="scss" global>
@@ -45,6 +93,15 @@
       &:not(:last-child) {
         margin-bottom: 5px;
       }
+    }
+    &__footer {
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      padding: 20px;
+      width: 100%;
+      /* z-index pulls the menu to the top on desktop devices */
+      z-index: 9000;
     }
   }
 </style>
