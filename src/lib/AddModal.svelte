69bd587ea2a65bcf2e34e41e0c619d8888e80f3a
Kacper Stawiński <stawinskidev@gmail.com>
2021-11-02 16:44:33 +0100

    feat(addmodal): rewrite view as component

---
+++ src/lib/AddModal.svelte
@@ -0,0 +1,176 @@
+<script lang="ts">
+  // props from Header component, then used by Modal
+  export let open: boolean
+  export let label: string
+  export let title: string
+
+  import { createEventDispatcher } from 'svelte'
+  import { navigate } from 'svelte-routing'
+  import {
+    Button,
+    TextInput,
+    InlineLoading,
+    InlineNotification,
+    NotificationActionButton
+  } from 'carbon-components-svelte'
+  import axios from 'axios'
+
+  import { db } from '../firebase'
+  import { collection, addDoc } from 'firebase/firestore'
+  import { Store } from '../hooks/Store'
+  import { Parsers } from '../hooks/Parsers'
+  import Modal from '../lib/Modal.svelte'
+
+  let ISBN: number
+  let isFetching = false
+
+  const dispatch = createEventDispatcher()
+
+  const notification = {
+    isVisible: false,
+    id: ''
+  }
+
+  const searchUsingGoogleBooks = () => new Promise((resolve, reject) => {
+    if (ISBN && ISBN.toString().length === 13) {
+      isFetching = true
+      axios.get(
+        `https://evening-citadel-78750.herokuapp.com/https://www.googleapis.com/books/v1/volumes?q=isbn:${ISBN}`,
+        { timeout: 10000 })
+        .then(data => data.data.items[0].volumeInfo)
+        .then(book => {
+          console.log(book)
+          resolve(book)
+          isFetching = false
+        })
+        .catch(error => reject(error))
+    }
+  })
+
+  const searchUsingISBN = () => new Promise((resolve, reject) => {
+    if (ISBN && ISBN.toString().length === 13) {
+      isFetching = true
+      axios.get(
+        `https://evening-citadel-78750.herokuapp.com/https://data.bn.org.pl/api/bibs.json?isbnIssn=${ISBN}`,
+        { timeout: 10000 })
+        .then(data => data.data.bibs[0])
+        .then(book => {
+          console.log(book)
+          resolve(book)
+          isFetching = false
+        })
+        .catch(error => reject(error))
+    }
+    else {
+      alert('Niepoprawna dlugosc numer isbn')
+      return
+    }
+  })
+
+  // addDoc(collection(db, 'books'), {
+  //   name: "Tokyo",
+  //   country: "Japan"
+  // });
+
+  const addBook = () => new Promise((resolve, reject) => {
+    searchUsingISBN()
+      .then(({ title, author, publicationYear }) => {
+        const book = {
+          uid: Store.getItem('uid'),
+          isbn: Number(ISBN),
+          title: Parsers.parseTitle(title),
+          author: Parsers.parseAuthor(author),
+          thumbnail: `https://covers.openlibrary.org/b/isbn/${ISBN}-L.jpg`,
+          addDate: new Date(),
+          publishedAt: Number(publicationYear)
+        }
+
+        // submit book
+        addDoc(collection(db, 'books'), book)
+          .then(({ id }) => {
+            console.log(`Document ID: ${id}`)
+            notification.id = id
+            notification.isVisible = true
+          })
+          .catch(error => console.error(error))
+      })
+      .catch(() => {
+        // second fetch using Google Books
+        searchUsingGoogleBooks()
+          .then(({ authors, publishedDate, title }) => {
+            const book = {
+              uid: Store.getItem('uid'),
+              isbn: Number(ISBN),
+              title: title,
+              author: authors[0],
+              thumbnail: `https://covers.openlibrary.org/b/isbn/${ISBN}-L.jpg`,
+              addDate: new Date(),
+              publishedAt: Number(publishedDate)
+            }
+
+            // submit book
+            addDoc(collection(db, 'books'), book)
+              .then(({ id }) => {
+                console.log(`Document ID: ${id}`)
+                notification.id = id
+                notification.isVisible = true
+              })
+              .catch(error => console.error(error))
+          })
+          .catch(error => console.error(error))
+      })
+  })
+
+  const closeModal = () => {
+    open = false
+    dispatch('close')
+  }
+</script>
+
+<Modal {open} {label} {title} on:close={ () => closeModal() } size="xs">
+  <div slot="content">
+    <p>Dodaj ksiażkę, wprowadzając numer ISBN znajdujący się z tyłu okładki.</p>
+
+    <div class="add__field">
+      <TextInput type="number" labelText="Numer ISBN" bind:value={ISBN} />
+    </div>
+
+    {#if isFetching}
+      <InlineLoading description="Szukam..." />
+    {/if}
+
+    {#if notification.isVisible}
+      <InlineNotification
+        kind="success"
+        subtitle="Książka została dodana."
+      >
+        <div slot="actions">
+          <NotificationActionButton
+            on:click={ () => navigate(`/book/${notification.id}`) }
+          >Zobacz</NotificationActionButton>
+        </div>
+      </InlineNotification>
+    {/if}
+  </div>
+
+  <svelte:fragment slot="footer">
+    <Button
+      kind="secondary"
+      on:click={ () => closeModal() }
+    >Anuluj</Button>
+
+    <Button
+      kind="primary"
+      on:click={addBook}
+      disabled={isFetching || (ISBN || 0).toString().length !== 13}
+    >Szukaj książki</Button>
+  </svelte:fragment>
+</Modal>
+
+<style lang="scss">
+  .add {
+    &__field {
+      margin: 10px 0;
+    }
+  }
+</style>
