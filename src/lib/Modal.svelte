b674b85b5c0c9bb8ac348f8073dde5b048d83a9d
Kacper Stawiński <stawinskidev@gmail.com>
2021-11-02 16:24:45 +0100

    feat(modal): create component

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  export let open: boolean
+  export let label: string
+  export let title: string
+  export let size = null
+
+  import { createEventDispatcher } from 'svelte'
+  import {
+    ComposedModal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
+  } from 'carbon-components-svelte'
+
+  const dispatch = createEventDispatcher()
+
+  const dispatchClose = () => dispatch('close')
+</script>
+
+<ComposedModal bind:open on:close={ () => dispatchClose() } {size}>
+  <ModalHeader {label} {title} />
+  <ModalBody hasForm>
+    <slot name="content"></slot>
+  </ModalBody>
+
+  {#if $$slots.footer}
+    <ModalFooter>
+      <slot name="footer"></slot>
+    </ModalFooter>
+  {/if}
+</ComposedModal>
